f0940192bc6ff0b27e8dffc169760f16f51a34cc
Juan Patricio Dugo <98497626+jpdugo@users.noreply.github.com>
2023-04-30 20:15:05 -0300

    function to calculate the tree

--- app/js/index.js
+++ app/js/index.js
@@ -1,9 +1,9 @@
 export function addClick(targetElement, shinyInputName) {
   $(document).ready(() => {
-   document.addEventListener('click', () => {
-     if ($(`#${targetElement}`).is(':visible')) {
+    document.addEventListener('click', () => {
+      if ($(`#${targetElement}`).is(':visible')) {
         Shiny.setInputValue(shinyInputName, Math.random(), { priority: 'event' });
       }
-  });
+    });
   });
 }
--- app/logic/options.R
+++ app/logic/options.R
@@ -1,3 +1,8 @@
+box::use(
+  glue[glue],
+  purrr
+)
+
 #' Calculate the price of a European call or put option
 #'
 #' This function calculates the price of a European call or put option using the binomial model.
@@ -35,4 +40,52 @@ price_option <- function(rf, time, n, p, u, d, s_0, k, type) {
   }
   # calculate present value
   sum(res) * exp(-(rf * time))
+}
+
+#' Calculate the matrix of the underlying asset value at each point in time
+#'
+#' This function creates a matrix of the underlying asset value at each point in time,
+#' based on the number of steps provided. It is used in the calculations of a
+#' Cox, Ross and Rubinstein binomial model.
+#'
+#' @param s_0 \code{numeric} The initial price of the underlying asset
+#' @param u \code{numeric} factor by which the underlying asset's price increases when it goes up
+#' @param d \code{numeric} factor by which the underlying asset's price decreases when it goes down
+#' @param n \code{integer} The number of time steps in the binomial model
+#'
+#' @return \code{data.frame} The matrix of the underlying asset value at each point in time
+#'
+#' @export
+#'
+#' @examples
+#' underlying_asset_value_matrix(100, 1.1, 0.9, 3)
+underlying_asset_value_matrix <- function(s_0, u, d, n) {
+  scenarios <- as.data.frame(
+    matrix(rep(NA, (n + 1) * (n + 1)), nrow = n + 1, ncol = n + 1)
+  )
+
+  # calculate scenarios
+  step <- 0
+  for (j in 0:n) {
+    for (i in 0:step) {
+      scenarios[(i + 1), (j + 1)] <- u^i * d^(step - i) * s_0
+    }
+    step <- step + 1
+  }
+
+  # Name the columns
+  nms <- purrr$map(
+    .x = c("step", "successes"),
+    .f = \(.x, .y) {
+      glue("{word}_{n}",
+        n    = seq(0, n, 1),
+        word = .x
+      )
+    }
+  )
+
+  colnames(scenarios) <- nms[[1]]
+  rownames(scenarios) <- nms[[2]]
+
+  return(scenarios)
 }
