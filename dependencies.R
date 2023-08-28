163f204a8b0206cb71ca6a2a817ba18e7a580946
Juan Patricio Dugo <98497626+jpdugo@users.noreply.github.com>
2023-08-27 21:17:26 -0300

    update packages

--- dependencies.R
+++ dependencies.R
@@ -1,6 +1,7 @@
 # This file allows packrat (used by rsconnect during deployment) to pick up dependencies.
+library(dplyr)
+library(languageserver)
 library(rhino)
 library(shiny.fluent)
 library(shinyjs)
-library(dplyr)
 library(stringr)
